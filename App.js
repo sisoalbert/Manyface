5334965d7b10baa9e4d76aae6d403a85a8ee1584
sisoalbert <sisoalbert@yahoo.com>
2023-01-22 16:49:29 +0200

    share image

--- App.js
+++ App.js
@@ -1,15 +1,86 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {WebView} from 'react-native-webview';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+} from 'react-native';
+import React, {useRef} from 'react';
+import ViewShot, {captureRef} from 'react-native-view-shot';
+import Share from 'react-native-share';
 
 const App = () => {
+  const ref = useRef();
+  const imagePlaceholder =
+    'https://images.unsplash.com/photo-1673209139602-30b6e9100131?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
+
+  const shareImage = async () => {
+    try {
+      const uri = await captureRef(ref, {
+        format: 'png',
+        quality: 0.7,
+      });
+      console.log('uri', uri);
+      await Share.open({url: uri});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <WebView source={{uri: 'https://google.com'}} />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>React Native Image Share</Text>
+        <ViewShot ref={ref}>
+          <Image
+            style={styles.generatedImage}
+            source={{
+              uri: imagePlaceholder,
+            }}
+          />
+        </ViewShot>
+
+        <TouchableOpacity style={styles.generateButton} onPress={shareImage}>
+          <Text style={styles.generateButtonText}>share</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Cochin',
+    textAlign: 'center',
+  },
+  generateButton: {
+    height: 50,
+    width: 300,
+    backgroundColor: 'black',
+    borderRadius: 10,
+    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  generateButtonText: {
+    color: 'white',
+  },
+  generatedImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  generatedImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
+});
